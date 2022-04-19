b827cb2657690fdac29fa0fe0c9bc9963660848c
John Stanford <johnwstanford@gmail.com>
2022-04-18 23:00:28 -0500

    Resolving dependencies

--- fetch_crates.py
+++ fetch_crates.py
@@ -6,10 +6,37 @@ Created on Sat Apr 16 10:55:58 2022
 @author: johnstanford
 """
 
+import json
 import os
 import requests
 import tarfile
 import toml
+
+def resolve_dep(name, version, remaining=None, greater_or_equal=True, path='./crates.io-index'):
+    
+    remaining_p = name if remaining == None else remaining
+    
+    print(path, remaining_p)
+    
+    possibilities = []
+
+    for subpath in os.listdir(path):
+        full_subpath = os.path.join(path, subpath)
+        if remaining_p.startswith(subpath):
+            return resolve_dep(name, version, 
+                remaining=remaining_p[len(subpath):], 
+                greater_or_equal=greater_or_equal, 
+                path=full_subpath)
+        if name == subpath and os.path.isfile(full_subpath):
+            for line in open(full_subpath):
+                dep = json.loads(line)
+                if dep['vers'].startswith(version) and greater_or_equal:
+                    possibilities.append((int(dep['vers'][len(version)+1:]), dep))
+                if dep['vers'] == version:
+                    return dep
+
+    if len(possibilities) > 0:
+        return max(possibilities, key=lambda d: d[0])[1]
 
 TEMP_TAR = 'temp.tar'
 CRATES_PATH = './crates'
@@ -44,3 +71,5 @@ tar.extractall(CRATES_PATH)
 manifest = toml.load(open('%s/%s-%s/Cargo.toml'%(CRATES_PATH, CRATE, VERSION)))
 
 print(manifest.keys())
+print(manifest['dependencies'])
+print(resolve_dep('serde_json', '1.0'))
